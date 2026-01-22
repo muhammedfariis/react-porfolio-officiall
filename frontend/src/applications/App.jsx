9cefbd788f1ba3fc253469426f5d8399aa4b667f
muhammedfariis <7591faris@gmail.com>
2026-01-22 16:06:53 +0530

    "hello"

--- frontend/src/applications/App.jsx
+++ frontend/src/applications/App.jsx
@@ -1,29 +1,31 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "../components/common/navbar";
-import Home from "../pages/home";
-import About from "../pages/about";
-import Project from "../pages/project";
-import Footer from "../components/common/footer";
-import Contact from "../pages/contact";
-import MouseMove from "../components/common/mousemove";
-function App() {
-  return (
-    <>
-      <Navbar />
+// import { Routes, Route } from "react-router-dom";
+// import Navbar from "../components/common/navbar";
+// import Home from "../pages/home";
+// import About from "../pages/about";
+// import Project from "../pages/project";
+// import Footer from "../components/common/footer";
+// import Contact from "../pages/contact";
+// import MouseMove from "../components/common/mousemove";
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
 
-        <Route path="/about" element={<About />} />
+//         <Route path="/about" element={<About />} />
 
-        <Route path="/project" element={<Project />} />
+//         <Route path="/project" element={<Project />} />
 
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-      <MouseMove />
-    </>
-  );
-}
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//       <Footer />
+//       <MouseMove />
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
